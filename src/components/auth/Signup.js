8fd28dba07071f778e2f504ae73b8a03f65383ef
AJ <aj@cubbyhole.net>
2017-02-22 08:50:02 +0000

    Fixed lint... :) Whoops!

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -57,7 +57,7 @@ class Signup extends Component {
             name="email"
             component={Input}
             placeholder="Email"
-            autoCapitalize={"none"}
+            autoCapitalize={'none'}
           />
         </Item>
 
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -26,7 +26,7 @@ const Input = (props) => {
     secureTextEntry,
     multiline,
     containerStyle,
-    ...otherProps,
+    ...otherProps
   } = props;
 
   return (
